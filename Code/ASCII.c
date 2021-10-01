49da5a1e7c326e9212367b925ba85e6f22501405
AnshDeep Yadav <59330323+AnshDeep24@users.noreply.github.com>
2021-10-01 11:19:42 +0530

    programme for ascii

---
+++ Code/ASCII.c
@@ -0,0 +1,13 @@
+// programme to fing ascii of any character//
+#include <stdio.h>
+int main() {  
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);  
+    
+    // %d displays the integer value of a character
+    // %c displays the actual character
+    printf("ASCII value of %c = %d", c, c);
+    
+    return 0;
+}
