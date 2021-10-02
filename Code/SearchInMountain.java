fd2520e4b08d692b27c704b070ab0e5251383cc5
Sushane <sushaneyadav@gmail.com>
2021-10-02 21:59:50 +0530

    Find in mountain array solution

---
+++ Code/SearchInMountain.java
@@ -0,0 +1,71 @@
+// https://leetcode.com/problems/find-in-mountain-array/
+
+import java.util.*;
+
+public class SearchInMountain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n ;i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        sc.close();
+        System.out.println(search(arr, target));
+
+    }
+    static int search(int[] arr, int target) {
+        int peak = peakIndexInMountainArray(arr);
+        int firstTry = orderAgnosticBS(arr, target, 0, peak);
+        if (firstTry != -1) {
+            return firstTry;
+        }
+        return orderAgnosticBS(arr, target, peak+1, arr.length - 1);
+    }
+
+     static int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid+1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return start;
+    }
+
+    static int orderAgnosticBS(int[] arr, int target, int start, int end) {
+        boolean isAsc = arr[start] < arr[end];
+
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+
+
