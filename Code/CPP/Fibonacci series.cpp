f64896bef83111c9d06908f42585dc6abf722c20
Raya513 <64854191+Raya513@users.noreply.github.com>
2021-10-04 16:27:18 +0530

    Create Fibonacci series.cpp

---
+++ Code/CPP/Fibonacci series.cpp
@@ -0,0 +1,34 @@
+//print fibonacci sequence upto Nth terms
+
+#include <iostream>
+using namespace std;
+
+void fib(int n)
+{
+  
+    int a=0,b=1,c;
+    for(int i=1;i<=n;i++)
+    {
+      
+        cout<<a<<" ";
+        c=a+b;
+        a=b;
+        b=c;
+      
+    }
+    //return;
+}
+
+
+int main()
+{
+  
+    int n;
+    cout<<"enter n"<<endl;
+    cin>>n;
+  
+fib(n);
+  
+    return 0;
+  
+}
