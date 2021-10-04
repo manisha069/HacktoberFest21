3bd7cc96b836fb29997b5cfb145d812a86be65ff
Aman Verma <amanverma.cs19@rvce.edu.in>
2021-10-04 18:41:45 +0530

    Adding the count inversions in array code in python

---
+++ Algorithms/Python/countNumberOfInversions.py
@@ -0,0 +1,61 @@
+# copyright Aman-Verma-28
+# count inversions in array code in python
+def countInversions(N,A):
+    """
+
+    :param N: size of the input array
+    :param A: input array in which we want to count the number of inversions
+    :return: count of inversions
+    """
+
+    def mergeSort(arr, n):
+        temp_arr = [0] * n
+        return _mergeSort(arr, temp_arr, 0, n - 1)
+
+
+
+
+    def _mergeSort(arr, temp_arr, left, right):
+        inv_count = 0
+        if left < right:
+           mid = (left + right) // 2
+           inv_count += _mergeSort(arr, temp_arr,left, mid)
+           inv_count += _mergeSort(arr, temp_arr,mid + 1, right)
+           inv_count += merge(arr, temp_arr, left, mid, right)
+        return inv_count
+
+
+
+    def merge(arr, temp_arr, left, mid, right):
+        i = left
+        j = mid + 1
+        k = left
+        inv_count = 0
+        while i <= mid and j <= right:
+            if arr[i] <= arr[j]:
+                temp_arr[k] = arr[i]
+                k += 1
+                i += 1
+            else:
+                temp_arr[k] = arr[j]
+                inv_count += (mid - i + 1)
+                k += 1
+                j += 1
+        while i <= mid:
+            temp_arr[k] = arr[i]
+            k += 1
+            i += 1
+        while j <= right:
+            temp_arr[k] = arr[j]
+            k += 1
+            j += 1
+        for loop_var in range(left, right + 1):
+            arr[loop_var] = temp_arr[loop_var]
+        return inv_count
+
+    return mergeSort(A,N)
+
+
+n = int(input("Enter the size of the array"))
+arr = list(map(int,input("Enter the array in which we want to count inversions").split()))
+print("Number of inversions in the given array are: ", countInversions(n,arr))
