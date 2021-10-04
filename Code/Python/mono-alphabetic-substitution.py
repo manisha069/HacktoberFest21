456de6fc99dbc333188a4990901d77f42c8885a5
Dhananjay Ambhore <54627513+Dhan1293@users.noreply.github.com>
2021-10-04 13:52:23 +0530

    mono-alphabetic substitution cipher coder - decoder
    This program gets the user input as a message and gives them the option to "encrypt" or "decrypt" according to which function is called and the given message is encrypted or decrypted with the given key in the program above.

---
+++ Code/Python/mono-alphabetic-substitution.py
@@ -0,0 +1,66 @@
+import sys
+
+letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+
+def main():
+    myMessage = input("Enter message to encrypt or decrypt: ")
+    myKey = 'QWERTYUIOPASDFGHJKLZXCVBNM'
+
+    # set to 'encrypt' or 'decrypt'
+    myMode = input("Enter 'encrypt' or 'decrypt' to perform task: ")
+
+    checkValidKey(myKey)
+
+    if myMode == 'encrypt':
+        translated = encryptMessage(myKey, myMessage)
+    elif myMode == 'decrypt':
+        translated = decryptMessage(myKey, myMessage)
+    print('Using key %s' % (myKey))
+    print('The %sed message is:' % (myMode))
+    print(translated)
+
+
+def checkValidKey(key):
+    keyList = list(key)
+    lettersList = list(letters)
+    keyList.sort()
+    lettersList.sort()
+    if keyList != lettersList:
+        sys.exit('There is an error in the key or symbol set.')
+
+
+def encryptMessage(key, message):
+    return translateMessage(key, message, 'encrypt')
+
+
+def decryptMessage(key, message):
+    return translateMessage(key, message, 'decrypt')
+
+
+def translateMessage(key, message, mode):
+    translated = ''
+    charsA = letters
+    charsB = key
+    if mode == 'decrypt':
+        # For decrypting, we can use the same code as encrypting. We
+        # just need to swap where the key and LETTERS strings are used.
+        charsA, charsB = charsB, charsA
+
+    # loop through each symbol in the message
+    for symbol in message:
+        if symbol.upper() in charsA:
+            # encrypt/decrypt the symbol
+            symIndex = charsA.find(symbol.upper())
+            if symbol.isupper():
+                translated += charsB[symIndex].upper()
+            else:
+                translated += charsB[symIndex].lower()
+        else:
+            # symbol is not in LETTERS, just add it
+            translated += symbol
+
+    return translated
+
+
+main()
