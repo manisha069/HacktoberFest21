d5f57ac149d9d9a6c6dac24fc29cbbbfe32a98f8
Nidita <64700758+Nidita@users.noreply.github.com>
2021-10-05 20:11:35 +0600

    Seive of Eratosthenes in C++ code

---
+++ Algorithms/C++/Seive of Eratosthenes.cpp
@@ -0,0 +1,38 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void SieveOfEratosthenes(int n)
+{
+
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+
+    for (int p = 2; p * p <= n; p++)
+    {
+
+        if (prime[p] == true)
+        {
+
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+
+
+    for (int p = 2; p <= n; p++)
+        if (prime[p])
+            cout << p << " ";
+}
+
+
+int main()
+{
+    int number;
+    cout<<"Enter Number:";
+    cin>>number;
+    cout<<"Prime number upto entered number"<<endl;
+    SieveOfEratosthenes(number);
+    return 0;
+}
+
