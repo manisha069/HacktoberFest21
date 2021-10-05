fb698a2bc0b3839bbb7ba09b0f6ec261b075f7fa
Manav Arora <imanavarora@gmail.com>
2021-10-05 12:53:42 +0530

    Added leetcode ques to check whether two strings are equal or not in JAVA

---
+++ Algorithms/JAVA/CompareStrings.java
@@ -0,0 +1,9 @@
+
+//LEETCODE SOLUTION
+//Check If Two String Arrays are Equivalent.java 
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+      return Arrays.stream(word1).reduce((a, b) -> a + b)
+          .equals(Arrays.stream(word2).reduce((a, b) -> a + b));
+    }
+  }
