f933d26e296ef851b55636cb218992d2d6fde607
Pranab1109 <70249715+Pranab1109@users.noreply.github.com>
2021-10-11 00:55:03 +0530

    Create pattern matching algo
    Pattern Matching algorithm using trie data structure.

---
+++ Algorithms/C++/Pattern-matching.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class TrieNode {
+   public:
+    char data;
+    TrieNode **children;
+    bool isTerminal;
+
+    TrieNode(char data) {
+        this->data = data;
+        children = new TrieNode *[26];
+        for (int i = 0; i < 26; i++) {
+            children[i] = NULL;
+        }
+        isTerminal = false;
+    }
+};
+
+class Trie {
+    TrieNode *root;
+
+   public:
+    int count;
+
+    Trie() {
+        this->count = 0;
+        root = new TrieNode('\0');
+    }
+
+    bool insertWord(TrieNode *root, string word) {
+        // Base case
+        if (word.size() == 0) {
+            if (!root->isTerminal) {
+                root->isTerminal = true;
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        // Small calculation
+        int index = word[0] - 'a';
+        TrieNode *child;
+        if (root->children[index] != NULL) {
+            child = root->children[index];
+        } else {
+            child = new TrieNode(word[0]);
+            root->children[index] = child;
+        }
+
+        // Recursive call
+        return insertWord(child, word.substr(1));
+    }
+
+    void insertWord(string word) {
+        if (insertWord(root, word)) {
+            this->count++;
+        }
+    }
+    bool search(string word){
+        return search(root,word);
+     }
+    bool search(TrieNode*root,string word) {
+        if(word.size()==0){
+            return true;
+        }
+        int index = word[0]-'a';
+        if(root->children[index]!=NULL){
+            return search(root->children[index],word.substr(1));
+        }
+        else
+        	return false;
+    }
+
+    bool patternMatching(vector<string> v, string pattern) {
+        for(int i=0;i<v.size();i++){
+            for(int j=0;j<v[i].size();j++)
+            insertWord(v[i].substr(j));
+        }
+        return search(pattern);
+    }
+};
+
+
+
+int main() {
+    Trie t;
+    int n;
+    cin >> n;
+    string pattern;
+    vector<string> vect;
+
+    for (int i = 0; i < n; ++i) {
+        string word;
+        cin >> word;
+        vect.push_back(word);
+    }
+    cin >> pattern;
+
+    cout << (t.patternMatching(vect, pattern) ? "true" : "false");
+}
