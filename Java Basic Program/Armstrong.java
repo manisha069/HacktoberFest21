42dfb2e01633ce8a3988416386a11890c75d030d
vidhidrana <56223752+vidhidrana@users.noreply.github.com>
2021-10-23 13:02:51 +0530

    Update Armstrong.java

--- Java Basic Program/Armstrong.java
+++ Java Basic Program/Armstrong.java
@@ -1,49 +1,50 @@
-import java.util.Scanner;  
-import java.lang.Math;  
-public class ArmstsrongNumberExample  
-{  
-//function to check if the number is Armstrong or not  
-static boolean isArmstrong(int n)   
-{   
-int temp, digits=0, last=0, sum=0;   
-//assigning n into a temp variable  
-temp=n;   
-//loop execute until the condition becomes false  
-while(temp>0)    
-{   
-temp = temp/10;   
-digits++;   
-}   
-temp = n;   
-while(temp>0)   
-{   
-//determines the last digit from the number      
-last = temp % 10;   
-//calculates the power of a number up to digit times and add the resultant to the sum variable  
-sum +=  (Math.pow(last, digits));   
-//removes the last digit   
-temp = temp/10;   
-}  
-//compares the sum with n  
-if(n==sum)   
-//returns if sum and n are equal  
-return true;      
-//returns false if sum and n are not equal  
-else return false;   
-}   
-//driver code  
-public static void main(String args[])     
-{     
-int num;   
-Scanner sc= new Scanner(System.in);  
-System.out.print("Enter the limit: ");  
-//reads the limit from the user  
-num=sc.nextInt();  
-System.out.println("Armstrong Number up to "+ num + " are: ");  
-for(int i=0; i<=num; i++)  
-//function calling  
-if(isArmstrong(i))  
-//prints the armstrong numbers  
-System.out.print(i+ ", ");  
-}   
-}  
+// Armstrong number in java using recursion
+import java.util.Scanner;
+class arm
+{
+   int n, s;
+   arm(int num1)
+   {
+      n = num1;
+      s = 0;
+   }
+   long powerOf(int a, int b)
+   {
+      if(b == 0)
+      {
+         return 1;
+      }
+      else
+      {
+         return a * powerOf(a, b - 1);
+      }
+   }
+   void executePowerOf()
+   {
+      int d = n, r;
+      long t = (long)s;
+      while(d > 0)
+      {
+         r = d % 10;
+         d = d / 10;
+         t = t + powerOf(r, 3);
+      }
+      if(n == t)
+      {
+         System.out.println(n+" is an Armstrong number.");
+      }
+      else
+      {
+         System.out.println(n+" is not an Armstrong number.");
+      }
+   }
+   public static void main(String[] args) 
+   {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Please enter the number: ");
+      int n = sc.nextInt();
+      arm obj = new arm(n);
+      obj.executePowerOf();
+      sc.close();
+   }
+}
